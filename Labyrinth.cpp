5372fc2bde066775350a14bca555a93511c754ce
alvinjwong <ajwong@uab.edu>
2024-10-24 13:15:25 -0500

    update?

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -1,4 +1,5 @@
 #include "Labyrinth.h"
+#include <vector>
 
 bool isPathToFreedom(MazeCell* start, const std::string& moves) {
    std::vector<Item> collectedItem; // not right idk syntax rn but im putting out ideas.
@@ -12,9 +13,28 @@ bool isPathToFreedom(MazeCell* start, const std::string& moves) {
         } else {
             //continue through pathing
         }
-    }
+    
 
     // for (something : moves) iterate and check what path
     //if moves->etc == nullptr, then its a false path
-   
 
+
+    MazeCell* currentLocation = start;
+
+    for (char path : moves) {
+        if (path == 'N') {
+            currentLocation = currentLocation->north;
+        } else if (path == 'E') {
+            currentLocation = currentLocation->east;
+        } else if (path == 'S') {
+            currentLocation = currentLocation->south;
+        } else if (path == 'W') {
+            currentLocation = currentLocation->west;
+        }
+    }
+
+    if (currentLocation == nullptr) {
+        return false; // i think this means that there is nothing there/no path there
+    }
+   
+}
