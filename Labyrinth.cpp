f5402fd5abf1c7c03d35c5352f60a3d1b6b188ad
alvinjwong <ajwong@uab.edu>
2024-10-24 00:29:02 -0500

    suggestion

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -1,8 +1,20 @@
 #include "Labyrinth.h"
 
 bool isPathToFreedom(MazeCell* start, const std::string& moves) {
-    /* TODO: Delete this comment and the remaining lines, then implement this function. */
-    (void) start;
-    (void) moves;
-    return false;
-}
+   std::vector<Item> collectedItem; // not right idk syntax rn but im putting out ideas.
+
+   if (whatsHere == "SPELLBOOK") {
+    collectedItem.push_back(SPELLBOOK);
+    } else if (whatsHere == "POTION") {
+        collectedItem.push_back(POTION);
+    } else if (whatsHere == "WAND") {
+        collectedItem.push_back(WAND);
+        } else {
+            //continue through pathing
+        }
+    }
+
+    // for (something : moves) iterate and check what path
+    //if moves->etc == nullptr, then its a false path
+   
+
